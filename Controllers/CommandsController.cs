e7b03c3f2e7b71e9608835b4289c741909d9de07
Michal Laszyn <mlaszyn@task.gda.pl>
2021-04-09 15:38:51 +0200

    DTO finished

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 using Commander.Models;
 using Commander.Data;
 using System.Collections.Generic;
+using AutoMapper;
+using Commander.Dtos;
+
 namespace Commander.Controllers
 {
     [Route("api/commands")]
@@ -9,23 +12,33 @@ namespace Commander.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly ICommanderRepo _repository;
-        public CommandsController(ICommanderRepo repository)
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands()
         {
             var commandItem = _repository.GetAllCommands();
-            return Ok(commandItem);
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItem));
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Command> GetCommandById(int id)
+        public ActionResult<CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
-            return Ok(commandItem);
+            if(commandItem != null)
+            {
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
+            }
+            else
+            {
+                return NotFound();
+            }   
         }
     }
 }
